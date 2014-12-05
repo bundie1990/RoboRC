bf7c68d6cff74077d835dd1b797c79afdce8e9a4
fduraibi <fadnix@gmail.com>
2014-12-04 23:41:03 -0500

    First commit

---
+++ app/src/main/java/net/fadvisor/roborc/MainActivity.java
@@ -0,0 +1,69 @@
+/**
+ * Created by Fahad Alduraibi on 12/4/14.
+ * fadvisor.net
+ */
+
+package net.fadvisor.roborc;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final View rl2 = findViewById(R.id.rl2);
+
+        rl2.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onGlobalLayout() {
+
+                int w = rl2.getWidth();
+                int h = rl2.getHeight();
+
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(h, w);
+                rl2.setLayoutParams(params);
+                rl2.setRotation(270.0f);
+                rl2.setTranslationX((w - h) / 2);
+                rl2.setTranslationY((h - w) / 2);
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN)
+                    rl2.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                else
+                    rl2.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
